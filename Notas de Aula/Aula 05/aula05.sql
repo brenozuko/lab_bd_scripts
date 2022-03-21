ff2644cbf2b8546646cce442be1899a91ef1317c
Breno Zukowski <brenozukotech@gmail.com>
2022-03-21 08:31:07 -0300

    Adiciona aula 05 com exercício 2

---
+++ Notas de Aula/Aula 05/aula05.sql
@@ -0,0 +1,79 @@
+CREATE TABLE tb_cliente_teste(
+id_cliente      INTEGER,
+ds_cliente      VARCHAR2(40),
+nm_cliente      VARCHAR2(40),
+valor           NUMERIC,
+fg_ativo        INTEGER,
+PRIMARY KEY (id_cliente)
+);
+
+CREATE OR REPLACE
+PROCEDURE manipula_dados(
+p_id_cliente IN tb_cliente_teste.id_cliente%TYPE,
+p_descricao  IN tb_cliente_teste.ds_cliente%TYPE,
+p_nome       IN tb_cliente_teste.nm_cliente%TYPE,
+p_valor      IN tb_cliente_teste.valor%TYPE,
+p_fg_ativo   IN tb_cliente_teste.fg_ativo%TYPE,
+p_opcao      IN CHAR)
+AS
+v_controle INTEGER;
+
+BEGIN 
+
+-- verifica a existência de tuplas na tb_cliente_teste
+SELECT COUNT(*) INTO v_controle
+FROM tb_cliente_teste
+WHERE id_cliente = p_id_cliente
+AND fg_ativo = 1;
+
+-- opção = I (INSERT)
+IF(p_opcao = 'I') THEN
+  IF(v_controle != 1) THEN
+    INSERT INTO tb_cliente_teste(id_cliente, ds_cliente, nm_cliente, valor, fg_ativo)
+    VALUES (p_id_cliente, p_descricao, p_nome, p_valor, p_fg_ativo);
+    COMMIT;
+    dbms_output.put_line('Cliente inserido com sucesso');
+    
+    ELSE
+    dbms_output.put_line('ID do cliente já existe');
+    
+  END IF;
+END IF;
+
+-- opção = U (UPDATE)
+IF(p_opcao = 'U') THEN
+  IF(v_controle = 1) THEN
+    UPDATE tb_cliente_teste SET ds_cliente      = p_descricao,
+                                nm_cliente      = p_nome,
+                                valor           = p_valor,
+                                fg_ativo        = p_fg_ativo
+    WHERE id_cliente = p_id_cliente
+    AND fg_ativo = 1;
+    
+    COMMIT;
+    dbms_output.put_line('Cliente atualizado com sucesso');
+  ELSE
+    dbms_output.put_line('ID do cliente não existe');
+  END IF;
+END IF;
+
+-- opção = D (DELETE)
+IF(p_opcao = 'D') THEN
+  IF (v_controle = 1) THEN
+    DELETE
+    FROM tb_cliente_teste
+    WHERE id_cliente = p_id_cliente
+    AND fg_ativo = 1;
+    
+    COMMIT;
+    
+    dbms_output.put_line('Cliente removido com sucesso');
+  ELSE
+    dbms_output.put_line('ID do cliente não existe');
+  END IF;
+END IF;
+
+EXCEPTION 
+  WHEN OTHERS THEN
+  ROLLBACK;
+END manipula_dados;
