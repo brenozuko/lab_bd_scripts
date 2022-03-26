9992f920b14fc977f58b4a60cc9b7a5c3dcbc15e
Breno Zukowski <brenozukotech@gmail.com>
2022-03-26 18:25:03 -0300

    Adiciona número 7

--- Scripts de Exercicio/Exercicio02.sql
+++ Scripts de Exercicio/Exercicio02.sql
@@ -898,3 +898,90 @@ VALUES
 
 SELECT *
 FROM tb_empregado;
+
+-- NÚMERO 7
+
+-- Inserção de dados table "tb_historico_funcao"
+INSERT INTO tb_historico_funcao
+VALUES (102
+, TO_DATE('13-JAN-1993', 'dd-MON-yyyy')
+, TO_DATE('24-JUL-1998', 'dd-MON-yyyy')
+, 'IT_PROG'
+, 60);
+
+INSERT INTO tb_historico_funcao
+VALUES (101
+, TO_DATE('21-SET-1989', 'dd-MON-yyyy')
+, TO_DATE('27-OUT-1993', 'dd-MON-yyyy')
+, 'AC_ACCOUNT'
+, 110);
+
+INSERT INTO tb_historico_funcao
+VALUES (101
+, TO_DATE('28-OUT-1993', 'dd-MON-yyyy')
+, TO_DATE('15-MAR-1997', 'dd-MON-yyyy')
+, 'AC_MGR'
+, 110);
+
+INSERT INTO tb_historico_funcao
+VALUES (201
+, TO_DATE('17-FEV-1996', 'dd-MON-yyyy')
+, TO_DATE('19-DEZ-1999', 'dd-MON-yyyy')
+, 'MK_REP'
+, 20);
+
+INSERT INTO tb_historico_funcao
+VALUES (114
+, TO_DATE('24-MAR-1998', 'dd-MON-yyyy')
+, TO_DATE('31-DEZ-1999', 'dd-MON-yyyy')
+, 'ST_CLERK'
+, 50);
+
+
+INSERT INTO tb_historico_funcao
+VALUES (122
+, TO_DATE('01-JAN-1999', 'dd-MON-yyyy')
+, TO_DATE('31-DEZ-1999', 'dd-MON-yyyy')
+, 'ST_CLERK'
+, 50
+);
+
+INSERT INTO tb_historico_funcao
+VALUES (200
+, TO_DATE('17-SET-1987', 'dd-MON-yyyy')
+, TO_DATE('17-JUN-1993', 'dd-MON-yyyy')
+, 'AD_ASST'
+, 90
+);
+
+INSERT INTO tb_historico_funcao
+VALUES (176
+, TO_DATE('24-MAR-1998', 'dd-MON-yyyy')
+, TO_DATE('31-DEZ-1998', 'dd-MON-yyyy')
+, 'SA_REP'
+, 80
+);
+
+INSERT INTO tb_historico_funcao
+VALUES (176
+, TO_DATE('01-JAN-1999', 'dd-MON-yyyy')
+, TO_DATE('31-DEZ-1999', 'dd-MON-yyyy')
+, 'SA_MAN'
+, 80
+);
+
+INSERT INTO tb_historico_funcao
+VALUES (200
+, TO_DATE('01-JUL-1994', 'dd-MON-yyyy')
+, TO_DATE('31-DEZ-1998', 'dd-MON-yyyy')
+, 'AC_ACCOUNT'
+, 90
+);
+
+SELECT *
+FROM tb_historico_funcao;
+
+-- Habilitando a restrição de integridade para a table “tb_departamento”
+ALTER TABLE tb_departamento
+ENABLE CONSTRAINT fk_gerente_depto;
+COMMIT;
