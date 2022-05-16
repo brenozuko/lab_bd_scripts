577ff69bb9f33019c331df85c4f43c2c4a1ad913
Breno Zukowski <brenozukotech@gmail.com>
2022-05-16 10:58:54 -0300

    Privilégios. esquemas e sinônimos

--- Notas de Aula P2/Aula 11/aula11-2.sql
+++ Notas de Aula P2/Aula 11/aula11-2.sql
@@ -27,3 +27,64 @@ GRANT CREATE SESSION,
 
 -- Apenas o privilégio concedido na query
 GRANT EXECUTE ANY PROCEDURE TO fernando WITH ADMIN OPTION;
+
+-- Conectado como Fernando em Exercicios
+GRANT EXECUTE ANY PROCEDURE TO sonia;
+
+GRANT EXECUTE ANY PROCEDURE TO PUBLIC;
+
+-- Mostra o usuário
+SHOW user;
+
+-- Seleciona privilégios do usuário
+SELECT *
+FROM user_sys_privs
+ORDER BY privilege;
+
+-- Ao criar um user como fernando
+CREATE USER roberto IDENTIFIED BY roberto1234;
+
+--ORA-01031: privilégios insuficientes
+DROP USER roberto;
+
+-- Se você revoga um privilégio qualquer de um usuário, e o mesmo passou este privilégio a outro usuário, o privilégio desse usuário se mantém. Só é válido pra privilégios de sistema, quando é de objeto revogam-se todos.
+
+REVOKE CREATE TABLE FROM fernando;
+
+-- CONECTADO COMO LOJA - PRIVILÉGIOS DE OBJETO
+
+GRANT SELECT, INSERT, UPDATE ON tb_produtos TO fernando;
+GRANT SELECT ON tb_funcionarios TO fernando;
+
+GRANT UPDATE(sobrenome, salario)
+ON tb_funcionarios TO fernando;
+
+GRANT SELECT ON tb_clientes TO fernando WITH GRANT OPTION;
+
+-- conectado como Fernando em loja 
+
+GRANT SELECT ON loja.tb_clientes TO sonia;
+
+-- CONECTADO COMO LOJA
+
+-- Verifica os privilégios concedidos em uma tabela específica
+SELECT *
+FROM user_tab_privs_made
+WHERE table_name = 'TB_PRODUTOS';
+
+-- Verifica privilégios concedidos em diversas tabelas
+SELECT *
+FROM user_col_privs_made;
+
+-- CONECTADO COMO FERNANDO
+
+-- Verifica quais privilégios fernando recebeu
+SELECT *
+FROM user_tab_privs_recd;
+
+
+-- CONECTADO COMO SYSTEM
+
+GRANT CREATE synonym TO fernando;
+
+-- SYNONYMS - TO BE ADDED
