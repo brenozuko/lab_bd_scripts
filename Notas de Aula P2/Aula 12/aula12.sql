55bb3eb037643528789c418c0574c5842182991a
Breno Zukowski <brenozukotech@gmail.com>
2022-05-23 09:48:37 -0300

    Finaliza auditoria e inicia tabelas

--- Notas de Aula P2/Aula 12/aula12.sql
+++ Notas de Aula P2/Aula 12/aula12.sql
@@ -41,3 +41,49 @@ AUDIT ALL ON loja.tb_funcionarios;
 -- CHECA QUANDO AS INSTRUÇÕES SÃO BEM E MAL SUCEDIDAS
 AUDIT UPDATE TABLE BY fernando WHENEVER NOT SUCCESSFUL;
 AUDIT INSERT TABLE WHENEVER NOT SUCCESSFUL;
+
+AUDIT CREATE USER, DROP USER WHENEVER SUCCESSFUL;
+
+AUDIT CREATE USER, DROP USER BY loja WHENEVER SUCCESSFUL;
+
+AUDIT SELECT ON loja.tb_produtos BY SESSION;
+AUDIT DELETE ON loja.tb_funcionarios BY ACCESS;
+AUDIT INSERT, UPDATE ON loja.tb_funcionarios BY ACCESS;
+
+-- TABELAS
+
+-- TABELAS TEMPORÁRIAS
+
+CREATE GLOBAL TEMPORARY TABLE tb_status_encomenda_temp(
+id_status_encomenda     INTEGER,
+status                  VARCHAR2(10),
+ultima_modificacao      DATE DEFAULT SYSDATE
+)
+ON COMMIT PRESERVE ROWS;
+
+INSERT INTO tb_status_encomenda_temp(id_status_encomenda, status)
+VALUES (1, 'Novo');
+
+SELECT * FROM tb_status_encomenda_temp;
+
+-- CRIAÇÃO DE TABELA COM SUBQUERY
+CREATE TABLE tb_funcionarios_teste
+AS
+  SELECT id_funcionario, nome, salario
+  FROM tb_funcionarios
+  WHERE id_funcionario = 3;
+
+-- CONSULTANDO TABELAS DENTRO DO CATÁLOGO USER_TABLES
+
+SELECT table_name,
+       tablespace_name,
+       temporary
+FROM user_tables
+WHERE table_name IN ('TB_STATUS_ENCOMENDA_2', 'TB_STATUS_ENCOMENDA_TEMP');
+
+-- CONSULTANDO CATÁLOGO USER_TAB_COLUMNS
+
+SELECT column_name, data_type, data_length,
+       data_precision, data_scale
+FROM user_tab_columns
+WHERE table_name = 'TB_PRODUTOS';
