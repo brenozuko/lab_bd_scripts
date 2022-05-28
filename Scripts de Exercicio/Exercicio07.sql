4964eed793a947c584c827d6362e643fbfadbc2d
Breno Zukowski <brenozukotech@gmail.com>
2022-05-28 16:49:19 -0300

    Finaliza Exercício 07

--- Scripts de Exercicio/Exercicio07.sql
+++ Scripts de Exercicio/Exercicio07.sql
@@ -20,8 +20,53 @@ OR UPPER(e.nome) LIKE 'A%';
 
 -- QUESTÃO 3
 
-
 SELECT e.nome, d.nm_departamento, l.cidade, l.estado
 FROM tb_empregado e
 INNER JOIN tb_departamento d ON(e.id_departamento = d.id_departamento)
 INNER JOIN tb_localizacao l ON(d.id_localizacao = l.id_localizacao)
+WHERE NVL(e.percentual_comissao, 0) > 0;
+
+
+-- QUESTÃO 4
+
+SELECT e.nome || ' trabalha para ' || NVL(g.nome, 'os acionistas')
+FROM tb_empregado e LEFT OUTER JOIN tb_empregado g ON(e.id_gerente = g.id_empregado)
+ORDER BY g.nome DESC;~
+
+-- QUESTÃO 5
+
+CREATE OR REPLACE PROCEDURE sp_get_emp(p_id IN integer)
+AS
+v_nome_completo            VARCHAR2(500);
+v_ds_funcao                VARCHAR2(500);
+v_controle                 INTEGER;
+
+BEGIN
+
+SELECT COUNT(*) INTO v_controle
+FROM tb_empregado 
+WHERE id_empregado = p_id;
+
+IF(v_controle = 1) THEN
+  SELECT e.nome || ' ' || e.sobrenome "Nome Completo", f.ds_funcao INTO v_nome_completo, v_ds_funcao
+  FROM tb_empregado e
+  INNER JOIN tb_funcao f ON(e.id_funcao = f.id_funcao)
+  WHERE e.id_empregado = p_id;
+
+  DBMS_OUTPUT.PUT_LINE('Nome: ' || v_nome_completo || ' Função: ' || v_ds_funcao); 
+ELSE 
+  DBMS_OUTPUT.PUT_LINE('Empregado ' || p_id || ' não localizado!');
+END IF;
+END sp_get_emp;
+
+SET serveroutput ON
+BEGIN
+sp_get_emp(200); -- invocando o stored procedure
+END;
+
+
+SET serveroutput ON
+BEGIN
+sp_get_emp(999); -- invocando o stored procedure
+END;
+
